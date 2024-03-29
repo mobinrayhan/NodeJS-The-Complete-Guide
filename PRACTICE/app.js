526601bb9094946ea95642a05caa74149216c031
Mobin <sdm241405@gmail.com>
2024-03-30 02:28:58 +0600

    again worked with  pug file

--- PRACTICE/app.js
+++ PRACTICE/app.js
@@ -1,27 +1,24 @@
-const express = require("express");
 const path = require("path");
 
+const express = require("express");
 const bodyParser = require("body-parser");
-
-const { rootPath } = require("./helper/rootPath");
-
-const { homeRouter } = require("./routes/home");
-const { messageRoute } = require("./routes/message");
-const { sendMessageRouter } = require("./routes/send-message");
-const { pageNotFound } = require("./routes/404");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(express.static(path.join(rootPath, "public")));
-
-const pug = require("pug");
 app.set("view engine", "pug");
+app.set("views", "views");
 
-app.use(sendMessageRouter);
-app.use(messageRoute);
-app.use(homeRouter);
-app.use(pageNotFound);
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+
+app.use("*", (req, res, next) => {
+  res.status(404).render("404");
+});
 
 app.listen(3000);
